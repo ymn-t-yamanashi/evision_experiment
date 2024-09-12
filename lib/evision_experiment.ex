e15ec508d2576deb85b0939e467814060caadfe8
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-12 23:37:00 +0900

    サンプルプログラム

--- lib/evision_experiment.ex
+++ lib/evision_experiment.ex
@@ -2,6 +2,8 @@ defmodule EvisionExperiment do
   @moduledoc """
   Documentation for `EvisionExperiment`.
   """
+  alias Evision, as: Cv
+  @tmp_image_path "/dev/shm/cv.png"
 
   @doc """
   Hello world.
@@ -13,6 +15,15 @@ defmodule EvisionExperiment do
 
   """
   def hello do
+    lenna_test_image_path = "test.png"
+    Cv.imread(lenna_test_image_path, flags: Cv.Constant.cv_IMREAD_GRAYSCALE())
+    |> display_image()
+
     :world
+  end
+
+  def display_image(binary) do
+    Cv.imwrite(@tmp_image_path, binary)
+    System.cmd("eog", [@tmp_image_path])
   end
 end
--- test/evision_experiment_test.exs
+++ test/evision_experiment_test.exs
@@ -1,8 +1,4 @@
 defmodule EvisionExperimentTest do
   use ExUnit.Case
   doctest EvisionExperiment
-
-  test "greets the world" do
-    assert EvisionExperiment.hello() == :world
-  end
 end
