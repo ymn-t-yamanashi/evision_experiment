4fc0e75ec7ba15fcf4e40f15aedf9d9926513fe5
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-12 23:14:12 +0900

    mix new evision_experiment

---
+++ lib/evision_experiment.ex
@@ -0,0 +1,18 @@
+defmodule EvisionExperiment do
+  @moduledoc """
+  Documentation for `EvisionExperiment`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EvisionExperiment.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/evision_experiment_test.exs
@@ -0,0 +1,8 @@
+defmodule EvisionExperimentTest do
+  use ExUnit.Case
+  doctest EvisionExperiment
+
+  test "greets the world" do
+    assert EvisionExperiment.hello() == :world
+  end
+end
